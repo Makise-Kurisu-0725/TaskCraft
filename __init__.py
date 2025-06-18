ad78ef3d687db595f738ab099696dbe8cdb0b1a2
dingfengshi <shidingfeng@outlook.com>
2025-06-19 00:03:17 +0800

    init push

---
+++ __init__.py
@@ -0,0 +1,5 @@
+from .src.gen_atomic_task import gen_atomic_tasks
+from .src.gen_depth_based_task import depth_extend
+from .src.gen_wdith_based_task import width_extend
+# from src.gen_width_based_task import
+
